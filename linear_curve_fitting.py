55031e45208ee1badb16e9acef94d2427c50a74f
pintupatra-DS <122179247+pintupatra-DS@users.noreply.github.com>
2023-01-11 14:24:04 +0100

    Update linear_curve_fitting.py

--- linear_curve_fitting.py
+++ linear_curve_fitting.py
@@ -2,14 +2,13 @@ import numpy as np
 from scipy.optimize import curve_fit,minimize
 import matplotlib.pyplot as plt
 
-#greate test data
+#generate test data
 
 A=10 #y=A+B*x
 B=0.75
 x_test=np.arange(0,40,1) #x-values
 x_error=np.random.rand(1,len(x_test))-0.5 #adding noise to the the data
 y_test=A+B*(x_test+x_error[0]) #y-values
-
 
 #code for fitting the generated data
 
@@ -26,7 +25,3 @@ p_opt = minimize(err, p_init).x
 
 print ('Original values:'+'A=',A,'and B=',B)
 print ('Fitted vales:'+'A=',p_opt[0],'and B=',p_opt[1])
-
-
-
-
